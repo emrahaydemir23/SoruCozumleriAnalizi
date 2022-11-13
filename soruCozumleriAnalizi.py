7f3dcd90cb08af7913111c5132b26742fe18c7cf
emrahaydemir23 <116636065+emrahaydemir23@users.noreply.github.com>
2022-11-13 21:27:33 +0300

    Update soruCozumleriAnalizi.py

--- soruCozumleriAnalizi.py
+++ soruCozumleriAnalizi.py
@@ -27,7 +27,7 @@ try:
     #df1 = df.groupby(["Ders Adı"])[["Doğru Sayısı", "Yanlış Sayısı", "Boş Sayısı", "Çözüm Süresi"]].sum()
 
     #df2 = df.sort_values(by='Zaman damgası', ascending=False)
-    df2 = ps.sqldf("Select [Ders Adı],max([Zaman damgası]) as [Zaman],sum([Çözüm Süresi])*1.0/sum([Toplam Sayı]) as [Ortalama Çözüm Süresi], count([Zaman damgası]) as [Çözüm Sayısı],sum([Toplam Sayı]) as [Bugüne Kadar Toplam Soru Sayısı] from df group by [Ders Adı] order by max([Zaman damgası]) desc")
+    #df2 = ps.sqldf("Select [Ders Adı],max([Zaman damgası]) as [Zaman],sum([Çözüm Süresi])*1.0/sum([Toplam Sayı]) as [Ortalama Çözüm Süresi], count([Zaman damgası]) as [Çözüm Sayısı],sum([Toplam Sayı]) as [Bugüne Kadar Toplam Soru Sayısı] from df group by [Ders Adı] order by max([Zaman damgası]) desc")
     #df2['Zaman'] = pd.to_datetime(df2['Zaman'])
     #df2["Geçen Gün Sayısı"] = (datetime.datetime.now()-df2["Zaman"]).dt.days
 
@@ -36,7 +36,7 @@ try:
     #df3.columns = ["Kitap Adı","Son Çözülme Zamanı"]
     df3 = ps.sqldf("Select [Kitap Adı],max([Zaman damgası]) as [Zaman],sum([Çözüm Süresi])*1.0/sum([Toplam Sayı]) as [Ortalama Çözüm Süresi], count([Zaman damgası]) as [Çözüm Sayısı],sum([Toplam Sayı]) as [Bugüne Kadar Toplam Soru Sayısı], (sum([Doğru Sayısı])-(sum([Yanlış Sayısı])/3))*100.0/(sum([Toplam Sayı])) as [Net Oranı] from df group by [Kitap Adı] order by max([Zaman damgası]) desc")
 
-    df4 = ps.sqldf(f"select [Ders Adı], sum([Doğru Sayısı]) as [Doğru], sum([Yanlış Sayısı]) as [Yanlış], sum([Boş Sayısı]) as [Boş], sum([Toplam Sayı]) as [Toplam], (sum([Doğru Sayısı])-(sum([Yanlış Sayısı])/3)) as [Net],(sum([Doğru Sayısı])-(sum([Yanlış Sayısı])/3))*100.0/(sum([Toplam Sayı])) as [Net Oranı],sum([Çözüm Süresi]) as [Toplam Süre],sum([Çözüm Süresi])*1.0/sum([Toplam Sayı]) as [Ortalama Süre],sum([Çözüm Süresi])*100.0/{genelToplamSure} as [Toplam Zaman Oranı], count([Zaman damgası]) as [Çözüm Sayısı],max([Zaman damgası]) as [Son Çözüm Zamanı] from df group by [Ders Adı]")
+    df4 = ps.sqldf(f"select [Ders Adı], sum([Doğru Sayısı]) as [Doğru], sum([Yanlış Sayısı]) as [Yanlış], sum([Boş Sayısı]) as [Boş], sum([Toplam Sayı]) as [Toplam], (sum([Doğru Sayısı])-(sum([Yanlış Sayısı])/3)) as [Net],(sum([Doğru Sayısı])-(sum([Yanlış Sayısı])/3))*100.0/(sum([Toplam Sayı])) as [Net Oranı],sum([Çözüm Süresi]) as [Toplam Süre],sum([Çözüm Süresi])*1.0/sum([Toplam Sayı]) as [Ortalama Süre],sum([Çözüm Süresi])*100.0/{genelToplamSure} as [Toplam Zaman Oranı], count([Zaman damgası]) as [Çözüm Sayısı],max([Zaman damgası]) as [Son Çözüm Zamanı] from df group by [Ders Adı] order by max([Zaman damgası]) desc")
     df4 = ps.sqldf("select * from df4 order by [Net Oranı]")
 
     df5 = ps.sqldf("select [Ders Adı],[Doğru Sayısı]*100.0/[Toplam Sayı] as [Doğru Oranı],[Yanlış Sayısı]*100.0/[Toplam Sayı] as [Yanlış Oranı],[Boş Sayısı]*100.0/[Toplam Sayı] as [Boş Oranı] from df")
@@ -59,9 +59,6 @@ try:
         #with right:
             #st.bar_chart(data=df4, x='Ders Adı', y='Net Oranı', width=0, height=0, use_container_width=True)
     
-    st.subheader("Derslerin En Son Çözülme Zamanları")
-    st.write(df2)
-
     st.subheader("Kitapların En Son Çözülme Zamanları")
     st.write(df3)
     
